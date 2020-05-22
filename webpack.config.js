96d39251c79592e686f684721fc4cb3e09a4c6e9
Jannik Richter <jannik811@gmail.com>
2020-05-22 08:09:15 +0200

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+module.exports = {
+  entry: {
+    bundle: path.join(__dirname, './src/index.js'),
+  },
+
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+  },
+
+  mode: process.env.NODE_ENV || 'development',
+
+  watchOptions: {
+    ignored: /node_modules|dist|\.js/g,
+  },
+
+  devtool: 'none',
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    plugins: [],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+      },
+    ],
+  },
+}
